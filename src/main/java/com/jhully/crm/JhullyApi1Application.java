80aa6906871c8eda2dacefabd970a6a7a20b24d5
JHONNYSMAX <jhonnysmax2@gmail.com>
2021-09-22 22:27:20 -0300

    Second commit

---
+++ src/main/java/com/jhully/crm/JhullyApi1Application.java
@@ -0,0 +1,13 @@
+package com.jhully.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhullyApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhullyApi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhully/crm/JhullyApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhully.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhullyApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
